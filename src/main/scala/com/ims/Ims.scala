b0160e1491b1ecabec995167496b36fb25b699b6
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-20 15:34:16 +0100

    Overloaded init() with init(u, p) for db connection, improved startup to ask to try again if db connection fails

--- src/main/scala/com/ims/Ims.scala
+++ src/main/scala/com/ims/Ims.scala
@@ -1,7 +1,7 @@
 package com.ims
 
 import java.util.Scanner
-import java.util.logging.Logger
+import java.util.logging.{Level, Logger}
 
 object Ims {
 
@@ -13,5 +13,21 @@ object Ims {
 
     LOGGER.info("PASSWORD:")
     val PASSWORD = getInput()
+
+    if (init(USERNAME, PASSWORD)) {
+
+    }
+    else {
+      LOGGER.log(Level.SEVERE, "Could not connect to DB")
+      LOGGER.info("Try again?")
+
+      if (getInput().toLowerCase() == "y") init()
+      System.exit(1) // Something went wrong connecting to the db
+    }
+  }
+
+  def init(username: String, password: String): Boolean = {
+    // TO-DO - Implement db connection
+    false
   }
 }
