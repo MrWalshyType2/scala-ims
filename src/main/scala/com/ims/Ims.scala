b3451be36b9311679a9aa6f531a574d35934d3f5
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-21 11:04:35 +0100

    Added CRUD enumeration, updated menu() in ims

--- src/main/scala/com/ims/Ims.scala
+++ src/main/scala/com/ims/Ims.scala
@@ -3,6 +3,7 @@ package com.ims
 import java.util.Scanner
 import java.util.logging.{Level, Logger}
 
+import com.ims.crud.CRUD
 import com.ims.domain.Domain
 
 object Ims {
@@ -35,5 +36,22 @@ object Ims {
 
   def menu(): Unit = {
     Domain.getDomains().foreach(domain => println(domain))
+    val domain: String = getInput()
+
+    if (domain.equalsIgnoreCase("EXIT")) System.exit(0)
+    else
+      LOGGER.info(s"What would you like to do with $domain?")
+      CRUD.getValues().foreach(value => println(value))
+
+      val operation: String = getInput()
+
+      if (operation.equalsIgnoreCase("CREATE")) ???
+      else if (operation.equalsIgnoreCase("READ")) ???
+      else if (operation.equalsIgnoreCase("UPDATE")) ???
+      else if (operation.equalsIgnoreCase("DELETE")) ???
+      else if (operation.equalsIgnoreCase("RETURN")) ???
+      else LOGGER.warning("Invalid input")
+
+    menu()
   }
 }
