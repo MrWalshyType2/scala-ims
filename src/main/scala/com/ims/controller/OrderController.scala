b3147f46dfeb3084a98cb81e9bb50fba8cbe62e3
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-22 21:55:05 +0100

    created order dao

--- src/main/scala/com/ims/controller/OrderController.scala
+++ src/main/scala/com/ims/controller/OrderController.scala
@@ -1,21 +1,12 @@
 package com.ims.controller
 
-import java.util.Scanner
 import java.util.logging.Logger
-
-import com.ims.dao.CustomerDAO
-import com.ims.domain.Customer
 
 object OrderController extends Controller {
 
-  private val SCANNER = new Scanner(Console.in)
   private val LOGGER = Logger.getLogger(OrderController.getClass.getSimpleName)
 
-  def getInput(): String = {
-    SCANNER.nextLine()
-  }
-
-  override def create: Unit = println("order create test")
+  override def create: Unit = {}
 
   override def readAll: Unit = ???
 
---
+++ src/main/scala/com/ims/dao/OrderDAO.scala
@@ -0,0 +1,12 @@
+package com.ims.dao
+
+object OrderDAO extends DAO[Order] {
+
+  override def create(order: Order): Unit = ???
+
+  override def readAll(): Unit = ???
+
+  override def update(order: Order): Unit = ???
+
+  override def delete(id: String): Unit = ???
+}
