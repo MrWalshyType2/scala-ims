f947f16db9ac874659a8e8c970406e1ef1850f93
MrWalshyType2 <mwalsh@academytrainee.com>
2020-09-23 15:26:30 +0100

    #SI-11 Added working update to order controller and dao, includes adding or deleting items from an order

--- src/main/scala/com/ims/controller/OrderController.scala
+++ src/main/scala/com/ims/controller/OrderController.scala
@@ -34,7 +34,7 @@ object OrderController extends Controller {
       val answer = getInputLoop(Seq("Y", "N"))
 
       if (answer == "N") {
-        OrderDAO.create(updatedOrder: Order)
+        OrderDAO.create(updatedOrder)
         LOGGER.info("Order created successfully!")
       }
       else addItemLoop(updatedOrder)
@@ -62,7 +62,24 @@ object OrderController extends Controller {
     OrderDAO.readAll()
   }
 
-  override def update: Unit = ???
+  override def update: Unit = {
+    LOGGER.info("Please enter your order ID: ")
+    val orderId = getInput()
+    val order = OrderDAO.readById(orderId)
+    LOGGER.info(s"${order.toString}\nRetrieved Order successfully")
+
+    var updatedOrder: Order = Order(BSONString(""), BSONString(""), BSONDocument())
+    LOGGER.info("Would you like to add items to your order? Y/N")
+    val answer = getInputLoop(Seq("Y", "N"))
+    if (answer.equalsIgnoreCase("Y")) updatedOrder = addItemLoop(order)
+    else {
+      LOGGER.info("Would you like to delete an item from your order? Y/N")
+      val answer = getInputLoop(Seq("Y", "N"))
+      if (answer.equalsIgnoreCase("Y")) updatedOrder = deleteItemLoop(order)
+    }
+    OrderDAO.update(updatedOrder)
+    LOGGER.info("Order updated successfully")
+  }
 
   override def delete: Unit = ???
 
@@ -71,5 +88,43 @@ object OrderController extends Controller {
     val userInput: String = getInput()
     if (validTerms.contains(userInput)) userInput
     else getInputLoop(validTerms)
+  }
+
+  @tailrec
+  def addItemLoop(order: Order): Order = {
+    LOGGER.info("ENTER ITEM ID:")
+    val itemId = getInput()
+    LOGGER.info("ENTER ITEM QUANTITY:")
+    var quantity = getInput()
+
+    val orderItems = order.orderItems ++ (itemId -> quantity.toInt)
+    orderItems.elements.foreach(element => {
+      println(s"ITEM: ${element.name}")
+      println(s"QUANTITY: ${BSONInteger.unapply(element.value).get}")
+    })
+
+    val updatedOrder = Order(order._id, order.customer_id, orderItems)
+
+    LOGGER.info("CONTINUE? Y/N")
+    val answer = getInputLoop(Seq("Y", "N"))
+
+    if (answer == "N") updatedOrder
+    else addItemLoop(updatedOrder)
+  }
+
+  @tailrec
+  def deleteItemLoop(order: Order): Order = {
+    LOGGER.info("ENTER ITEM ID:")
+    val itemId = getInput()
+
+    val orderItems = order.orderItems -- itemId
+    val updatedOrder = Order(order._id, order.customer_id, orderItems)
+
+    LOGGER.info(updatedOrder.toString)
+    LOGGER.info("Would you like to delete another item? Y/N")
+    val answer = getInputLoop(Seq("Y", "N"))
+
+    if (answer.equalsIgnoreCase("N")) updatedOrder
+    else deleteItemLoop(updatedOrder)
   }
 }
--- src/main/scala/com/ims/dao/OrderDAO.scala
+++ src/main/scala/com/ims/dao/OrderDAO.scala
@@ -3,12 +3,13 @@ package com.ims.dao
 import java.util.logging.Logger
 
 import com.ims.DBConnection.{orderCollection, orderReader, orderWriter}
-import com.ims.domain.{Order}
+import com.ims.domain.Order
 import reactivemongo.api.Cursor
-import reactivemongo.api.bson.document
+import reactivemongo.api.bson.{BSONDocument, BSONObjectID, BSONString, document}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
 object OrderDAO extends DAO[Order] {
@@ -34,7 +35,36 @@ object OrderDAO extends DAO[Order] {
     }
   }
 
-  override def update(order: Order): Unit = ???
+  override def update(order: Order): Unit = {
+    val selector = document(
+      "_id" -> order._id
+    )
+    orderCollection.flatMap(_.update.one(selector, order).map(_.n))
+  }
 
   override def delete(id: String): Unit = ???
+
+  def readById(id: String): Order = {
+    val query = BSONDocument("_id" -> id)
+
+    val orders: Future[List[Order]] = orderCollection.flatMap(_.find(query)
+      .cursor[Order]()
+      .collect[List](1, Cursor.FailOnError[List[Order]]()))
+
+    orders andThen {
+      case Success(value) => {
+//        value.foreach(order => LOGGER.info(order.toString))
+      }
+      case Failure(e) => {
+        LOGGER.severe(e.getMessage)
+      }
+    }
+
+    var order: Order = Order(BSONString(""), BSONString(""), BSONDocument())
+    Await.result(orders, Duration.Inf) {
+      order = orders.value.get.get.head
+      0
+    }
+    order
+  }
 }
